647e52e5e40190003ca0e82dfc4bce4487fdc63d
Yashraj <yashsharmau9@gmail.com>
2023-12-24 23:18:23 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<div>Page Not Found !</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
